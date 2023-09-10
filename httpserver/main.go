d91e6cee407ffa716436bf4a394986a184fc6a3e
aoi1 <tkhsaoi88@gmail.com>
2023-09-10 16:12:59 +0900

    Revert main.go for v1.0.0

--- httpserver/main.go
+++ httpserver/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
@@ -15,29 +14,12 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
 		fmt.Fprintf(w, "Hello, world! Let's learn Kubernetes!")
 	})
-
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/healthz" {
-			http.NotFound(w, r)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "OK")
-		log.Printf("Health Status OK")
-	})
-
-	http.Handle("/metrics", promhttp.Handler())
 
 	log.Printf("Starting server on port %s\n", port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
